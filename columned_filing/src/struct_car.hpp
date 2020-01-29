ac8fd669080e61d1e8180b65d8c3058eb39976f2
Karl McBride <karlmcbride1992@gmail.com>
2020-01-30 00:00:43 +1100

    [COLUMNED_FILING] Implement file_data_t struct

--- columned_filing/src/struct_car.hpp
+++ columned_filing/src/struct_car.hpp
@@ -3,36 +3,70 @@
 
 #include <string>
 
+template <typename value_type> struct file_data_t
+{
+    std::string column_name;
+    int column_index;
+    value_type value;
+
+    bool operator < (const file_data_t& rhs_struct) const
+    {
+        return (value < rhs_struct.value);
+    }
+
+    bool operator > (const file_data_t& rhs_struct) const
+    {
+        return (rhs_struct.value < value);
+    }
+
+    bool operator == (const file_data_t& rhs_struct) const
+    {
+        return (value == rhs_struct.value);
+    }
+};
+
 struct struct_car_t
 {
-    std::string manufacturer;
-    std::string model;
-    int horsepower;
-    int door_count;
+    file_data_t<std::string> manufacturer;
+    file_data_t<std::string> model;
+    file_data_t<int> horsepower;
+    file_data_t<int> door_count;
 
     struct_car_t(std::string _manufacturer, std::string _model, int _horsepower, int _door_count)
     {
-        manufacturer = _manufacturer;
-        model = _model;
-        horsepower = _horsepower;
-        door_count = _door_count;
+        manufacturer.column_index = 0;
+        manufacturer.column_name = "MANUFACTURER";
+        manufacturer.value = _manufacturer;
+
+        model.column_index = 1;
+        model.column_name = "MODEL";
+        model.value = _model;
+
+        horsepower.column_index = 2;
+        horsepower.column_name = "HORSEPOWER";
+        horsepower.value = _horsepower;
+
+        door_count.column_index = 3;
+        door_count.column_name = "DOOR_COUNT";
+        door_count.value = _door_count;
     }
 
     std::string as_string(void) const
     {
-        std::string struct_as_string = manufacturer + ":" + model + ":" + std::to_string(horsepower) + ":" + std::to_string(door_count);
+        std::string struct_as_string = manufacturer.value + ":" + model.value + ":"
+                                        + std::to_string(horsepower.value) + ":" + std::to_string(door_count.value);
         return struct_as_string;
     }
 
-    bool operator==(const struct_car_t& rhs_struct) const
+    bool operator == (const struct_car_t& rhs_struct) const
     {
-       return (manufacturer == rhs_struct.manufacturer
-                && model == rhs_struct.model
+       return (manufacturer.value == rhs_struct.manufacturer.value
+                && model.value == rhs_struct.model.value
                 && horsepower == rhs_struct.horsepower
                 && door_count == rhs_struct.door_count);
     }
 
-    bool operator<(const struct_car_t& rhs_struct) const
+    bool operator < (const struct_car_t& rhs_struct) const
     {
         if (manufacturer < rhs_struct.manufacturer)
         {
@@ -83,7 +117,7 @@ struct struct_car_t
         }
     }
 
-    bool operator>(const struct_car_t& rhs_struct) const
+    bool operator > (const struct_car_t& rhs_struct) const
     {
         return (rhs_struct.manufacturer < manufacturer);
     }
