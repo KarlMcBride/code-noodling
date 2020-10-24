8b870f5c1a2acf37d81ad0ee3bb590505b79c074
Karl McBride <karlmcbride1992@gmail.com>
2020-10-24 22:24:40 +0100

    [SNIPPETS] Add chrono example

---
+++ snippets/c++/chrono/main.cpp
@@ -0,0 +1,57 @@
+// Source:
+//      https://cplusplus.com/reference/chrono/steady_clock/
+//      https://cplusplus.com/reference/chrono/system_clock/
+
+
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+void timer()
+{
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+    std::cout << "printing out 1000 stars...\n";
+    for (int i=0; i<1000; ++i)
+    {
+        std::cout << "*";
+    }
+
+    std::cout << std::endl;
+
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+    std::cout << "It took me " << time_span.count() << " seconds.";
+    std::cout << std::endl;
+}
+
+void real_time_clock()
+{
+    std::chrono::duration<int,std::ratio<60*60*24> > one_day (1);
+
+    std::chrono::system_clock::time_point today = std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point tomorrow = today + one_day;
+
+    std::time_t tt;
+
+    tt = std::chrono::system_clock::to_time_t ( today );
+    std::cout << "today is: " << ctime(&tt);
+
+    tt = std::chrono::system_clock::to_time_t ( tomorrow );
+    std::cout << "tomorrow will be: " << ctime(&tt);
+
+}
+
+int main ()
+{
+    timer();
+    real_time_clock();
+
+    uint64_t seconds_since_epoc = std::chrono::system_clock::now().time_since_epoch() / std::chrono::seconds(1);
+    std::cout << "seconds_since_epoc: " << seconds_since_epoc << std::endl;
+
+    return 0;
+}
