1d4b99a52746fc44ab4ea43c009ed811bf34f6be
Karl McBride <karlmcbride1992@gmail.com>
2020-10-30 17:34:14 +0000

    [SNIPPETS] Add c++ console formatting example

---
+++ snippets/c++/console_formatting/build_run.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -o example.ex  && ./example.ex
---
+++ snippets/c++/console_formatting/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "\e[0;31m31\e[0m" << std::endl;
+    std::cout << "\e[0;32m32\e[0m" << std::endl;
+    std::cout << "\e[0;33m33\e[0m" << std::endl;
+    std::cout << "\e[0;34m34\e[0m" << std::endl;
+    std::cout << "\e[0;35m35\e[0m" << std::endl;
+    std::cout << "\e[0;36m36\e[0m" << std::endl;
+    std::cout << "\e[0;37m37\e[0m" << std::endl;
+    std::cout << "\e[0;38m38\e[0m" << std::endl;
+    std::cout << "\e[0;39m39\e[0m" << std::endl;
+    std::cout << "\e[0;40m40\e[0m" << std::endl;
+    std::cout << "\e[0;41m41\e[0m" << std::endl;
+    std::cout << "\e[0;42m42\e[0m" << std::endl;
+    std::cout << "\e[0;43m43\e[0m" << std::endl;
+    std::cout << "\e[0;44m44\e[0m" << std::endl;
+    std::cout << "\e[0;45m45\e[0m" << std::endl;
+    std::cout << "\e[0;46m46\e[0m" << std::endl;
+    std::cout << "\e[0;47m47\e[0m" << std::endl;
+    std::cout << "\e[0;48m48\e[0m" << std::endl;
+    std::cout << "\e[0;49m49\e[0m" << std::endl;
+}
