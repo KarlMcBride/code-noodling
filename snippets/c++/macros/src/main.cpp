16c0fa001610253aab2c755d97b09c8134608582
Karl McBride <karlmcbride1992@gmail.com>
2020-11-07 13:02:14 +0000

    [SNIPPETS] Add relative path output to macro

--- snippets/c++/macros/src/main.cpp
+++ snippets/c++/macros/src/main.cpp
@@ -5,23 +5,33 @@
 #include <stdio.h>
 #define EMPTY
 
-#define MACRO_LOG() macro_log(__FILE__, __FUNCTION__, __LINE__)
+#define MACRO_LOG(_strip_path) macro_log(__FILE__, __FUNCTION__, __LINE__, _strip_path)
 
-void macro_log(std::string _file, std::string _function, int _line)
+void macro_log(std::string _file, std::string _function, int _line, bool _strip_path)
 {
-    std::cout << "macro_log: " << _file << ", " << _function << ", " << _line << std::endl;
-    std::cout << __BASE_FILE__ << std::endl;
-    std::cout << __FILE__ << std::endl;
+    if (_strip_path)
+    {
+        const std::string project_folder = "src";
+        size_t position = _file.find(project_folder);
+        _file = _file.substr(position);
+        std::cout << "macro_log stripped path: " << _file << ":" << _function << ":" << _line << std::endl;
+    }
+    else
+    {
+        std::cout << "macro_log full path: " << _file << ":" << _function << ":" << _line << std::endl;
+    }
 }
 
 void second_function()
 {
-    MACRO_LOG();
+    MACRO_LOG(true);
+    MACRO_LOG(false);
 }
 
 int main()
 {
-    MACRO_LOG();
+    MACRO_LOG(true);
+    MACRO_LOG(false);
 
     second_function();
 
