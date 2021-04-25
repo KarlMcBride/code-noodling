8775785e73ce1905eaf29ea90f63837805ca6733
Karl McBride <karlmcbride1992@gmail.com>
2021-04-25 21:12:06 +0100

    [SNIPPETS] Add basic Python matplotlib chart example

---
+++ snippets/python/matplotlib/basic_chart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_points = np.array([1, 2, 6, 8])
+y_points = np.array([3, 8, 1, 10])
+
+plt.plot(x_points, y_points, marker='o')
+plt.xlabel("X Axis Label")
+plt.ylabel("Y Axis Label")
+plt.grid()
+plt.show()
